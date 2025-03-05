ca2c7ed365b70392f1438c35971bae6843497c87
elamuhombe <emuhombe@gmail.com>
2025-03-05 20:31:17 +0300

    chore: Export validation schemas from index file

---
+++ backend/src/validators/index.ts
@@ -0,0 +1,13 @@
+//src/validators/index.ts
+
+export {ActivityValidator} from "./activity.validator"
+export {AttendanceValidator} from "./attendance.validator"
+export {ClassValidator} from "./class.validator"
+export {ExamValidator} from "./exam.validator"
+export {InvoiceValidator} from "./invoice.validator"
+export {PaymentValidator} from "./payment.validator"
+export {SchoolValidator} from "./school.validator"
+export {StudentValidator} from "./student.validator"
+export {StudentAttendanceValidator} from "./studentAttendance.validator"
+export {SubjectValidator} from "./subject.validator"
+export {TransportValidator} from "./transport.validator"
