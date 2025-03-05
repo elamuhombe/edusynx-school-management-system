c15eb2de36b7266b6c590f3cd24b1d3bc3f48ccf
elamuhombe <emuhombe@gmail.com>
2025-03-05 19:31:52 +0300

    refactor: update activity.validator.ts to use ActivityValidator and handle ObjectId type for school

--- backend/src/validators/activity.validator.ts
+++ backend/src/validators/activity.validator.ts
@@ -1,14 +1,15 @@
 //src/validators/activity.validator.ts
 
 import { z } from "zod";
-import { SchoolSchema } from "./school.validator";
+import { SchoolValidator } from "./school.validator";
+import { ISchool } from "../types";
+import { Types } from "mongoose";
 
-const ActivitySchema = z.object({
-    school: SchoolSchema,
+const ActivityValidator = z.object({
+    school: z.union([z.instanceof(Types.ObjectId), SchoolValidator]),
     activityName: z.string(),
     activityFeeAmount: z.number(),
   });
 
-  export type IActivity = z.infer<typeof ActivitySchema>;
-
-export { ActivitySchema };
+  export type IActivity = z.infer<typeof ActivityValidator>;
+export{ActivityValidator}
