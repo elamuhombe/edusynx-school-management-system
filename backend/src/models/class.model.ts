dc2169dd327a2c9786b5d231fd617e149798ea6c
elamuhombe <emuhombe@gmail.com>
2025-03-05 11:26:06 +0300

    feat: Define Class model with schema for school, className, and year

--- backend/src/models/class.model.ts
+++ backend/src/models/class.model.ts
@@ -1,1 +1,14 @@
 //src/models/class.model.ts
+
+import mongoose, { Model, Types } from "mongoose";
+import { IClass } from "../types";
+
+const classSchema = new mongoose.Schema<IClass>({
+    school: {type:Types.ObjectId},
+    className: {type: String, required: true},
+    year: {type: Number, required: true}
+})
+
+const Class: Model<IClass>=mongoose.model("Class", classSchema)
+
+export default Class
