e7ca19800a1c9bc145541064d0bd296531869901
elamuhombe <emuhombe@gmail.com>
2025-03-05 13:37:37 +0300

    feat: add enums for Gender, Role, and PaymentMode

---
+++ backend/src/types/enums.ts
@@ -0,0 +1,22 @@
+//src/types/enums.ts
+export enum Gender {
+    BOY = "boy",
+    GIRL = "girl",
+    MALE = "male",
+    FEMALE = "female",
+  }
+  
+  export enum Role {
+    ADMIN = "admin",
+    HEADTEACHER = "headTeacher",
+    SCHOOLADMIN = "schoolAdmin",
+    ACCOUNTANT = "accountant",
+    GUARDIAN = "guardian",
+  }
+  
+  export enum PaymentMode {
+    MPESA = "mpesa",
+    BANK_CASH_DEPOSIT = "bankCashDeposit",
+    BANK_CHEQUE_DEPOSIT = "bankChequeDeposit",
+  }
+  
