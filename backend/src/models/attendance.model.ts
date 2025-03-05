96510a9a96e2f640190f592f7fa26aebc4cdd3ee
elamuhombe <emuhombe@gmail.com>
2025-03-05 15:00:33 +0300

    feat: Add attendance schema with student attendance data

--- backend/src/models/attendance.model.ts
+++ backend/src/models/attendance.model.ts
@@ -1,1 +1,21 @@
 //src/models/attendance.model.ts
+
+import mongoose, { Model, Types } from "mongoose";
+import { IAttendance } from "../types/school";
+
+const attendanceSchema = new mongoose.Schema<IAttendance>({
+    class:{type: Types.ObjectId, required: true},
+    date:{type: Date, required: true},
+    studentAttendances: [{ // Array containing individual student attendance data
+        studentName: {
+            type: String, // Store the student's name as a string
+            ref: "Student", // Reference to the Student model
+            required: true //
+          },
+        isPresent: { type: Boolean, required: true } 
+    }]
+})
+
+const Attendance: Model<IAttendance> = new mongoose.Model("Attendance", attendanceSchema)
+
+export {Attendance}
