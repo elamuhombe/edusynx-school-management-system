7d215e49cfddf405aa4c314d0d48a405476b0b1e
elamuhombe <emuhombe@gmail.com>
2025-03-05 15:52:29 +0300

    feat: update models export in index.ts to include all models

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -2,3 +2,12 @@
 
 export { default as School } from "./school.model";
 export { default as Class } from "./class.model";
+export {default as Student} from "./student.model"
+export {default as Payment} from "./payment.model"
+export {default as Attendance} from "./attendance.model"
+export {default as User} from "./user.model"
+export {default as Exam} from "./exam.model"
+export {default as Activity} from "./activity.model"
+export {default as Subject} from "./subject.model"
+export {default as Transport} from "./transport.model"
+export {default as Tuition} from "./tuition.model"
