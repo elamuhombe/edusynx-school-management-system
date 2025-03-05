30c3cf9a17801d93f553a7e104c49ea8b6c51f36
elamuhombe <emuhombe@gmail.com>
2025-03-05 13:33:51 +0300

    feat: add Mongoose model for Student entity

--- backend/src/models/student.model.ts
+++ backend/src/models/student.model.ts
@@ -1,1 +1,27 @@
 //src/models/student.model.ts
+
+import mongoose, { Model, Types } from "mongoose";
+import { IStudent } from "../types";
+import { Gender } from "../types/enums";
+
+const studentSchema = new mongoose.Schema<IStudent>({
+    studentId:{type: String},
+    school:{type:Types.ObjectId},
+    studentFirstName:{type: String, required: true},
+    studentMiddleName:{type: String, required: true},
+    studentLastName:{type: String, required: true},
+    gender: { type: String, enum: [Gender.BOY, Gender.GIRL], required: true },
+    dateOfBirth:{type:Date, required: true},
+    clas:{type:Types.ObjectId},
+    previousSchool:{type: String},
+    adm:{type: Number},
+    guardian:{type: Types.ObjectId},
+    exam:{type: Types.ObjectId},
+    activity:{type: Types.ObjectId},
+    transport:{type: Types.ObjectId}
+
+})
+
+const Student: Model<IStudent> = new mongoose.Model("Student", studentSchema)
+
+export {Student}
