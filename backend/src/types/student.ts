0ead9c1d1273c6f068e39bfe03424bd57deb91b3
elamuhombe <emuhombe@gmail.com>
2025-03-05 13:59:48 +0300

    Define IStudent interface for student details
    - Added  interface to represent student information including personal details, school, class, guardian, exams, and related activities and transport.
    - Integrate references to related entities like , , , , , and .

---
+++ backend/src/types/student.ts
@@ -0,0 +1,22 @@
+// src/types/student.ts
+import { Types } from "mongoose";
+import { IClass, IExam, ISchool, ITransport, IActivity } from "./school";
+import { IUser } from "./user";
+import { Gender } from "./enums";
+
+export interface IStudent {
+  studentId?: string;
+  school: string | ISchool;
+  studentFirstName: string;
+  studentMiddleName: string;
+  studentLastName: string;
+  gender: Gender;
+  dateOfBirth: Date;
+  clas: Types.ObjectId | IClass;
+  previousSchool: string;
+  adm: number;
+  guardian: Types.ObjectId | IUser;
+  exam: Types.ObjectId | IExam;
+  activity?: Types.ObjectId | IActivity;
+  transport?: Types.ObjectId | ITransport
+}
