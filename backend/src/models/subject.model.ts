edcd6a49d418282d0cb454bfcb1d8c004c8af35f
elamuhombe <emuhombe@gmail.com>
2025-03-05 16:00:29 +0300

    feat: define Subject model with subjectName, department, and teacher reference

--- backend/src/models/subject.model.ts
+++ backend/src/models/subject.model.ts
@@ -1,1 +1,18 @@
 //src/models/subject.model.ts
+
+import mongoose, { Model, Types } from "mongoose";
+import { ISubject } from "../types/school";
+
+const subjectSchema = new mongoose.Schema<ISubject>({
+    subjectName: { type: String, required: true },
+  department: { type: String, required: true },
+  teacher: { 
+    type: Types.ObjectId, 
+    ref: 'User', 
+    required: true
+  }
+});
+
+const Subject: Model<ISubject> = mongoose.model<ISubject>("Subject", subjectSchema);
+
+export default Subject;
